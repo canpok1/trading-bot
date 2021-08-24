f0cb947fd86a3b06a791d9f57223db7867d7f543
canpok1 <6isstrong@gmail.com>
2021-08-24 13:36:30 +0000

    bot_statusesテーブルのカラムにコメント追加しNULL不可に変更

--- build/ddl/V1.0.14__add_pair.sql
+++ build/ddl/V1.0.14__add_pair.sql
@@ -1,4 +1,4 @@
 ALTER TABLE bot_statuses
-ADD pair VARCHAR(15) DEFAULT NULL AFTER bot_name,
+ADD pair VARCHAR(15) NOT NULL COMMENT 'all or pair string (e.g.: btc_jpy)' AFTER bot_name ,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (bot_name, pair, type);
